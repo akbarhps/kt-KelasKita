8e4002ddce7258d70632c81d9207fd44de23796c
akbarhps <akbarputrasrg@gmail.com>
2021-02-20 06:45:14 +0700

    🚀 Initial commit

---
+++ app/src/main/java/com/charuniverse/kelasku/Kelasku.kt
@@ -0,0 +1,7 @@
+package com.charuniverse.kelasku
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Kelasku : Application()
---
+++ app/src/main/java/com/charuniverse/kelasku/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.charuniverse.kelasku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
